0426e9508c6f70c58b37c0d9048227ee58fd4767
Jin <j@shopar.dev>
2021-08-13 10:45:11 +0700

    update AC

--- src/v1/devices/AC.ts
+++ src/v1/devices/AC.ts
@@ -2,15 +2,48 @@ import {default as AirConditioner, FanSpeed} from '../../devices/AirConditioner'
 import {CharacteristicValue} from 'homebridge';
 import {ACOperation} from '../transforms/AirState';
 import {Device} from '../../lib/Device';
+import {RangeValue} from '../../lib/DeviceModel';
 
 export default class AC extends AirConditioner {
+
+  protected createHeaterCoolerService() {
+    const {
+      Characteristic,
+    } = this.platform;
+    const device: Device = this.accessory.context.device;
+
+    super.createHeaterCoolerService();
+
+    const currentTemperatureValue = device.deviceModel.value('TempCur') as RangeValue;
+    this.service.getCharacteristic(Characteristic.CurrentTemperature)
+      .setProps({
+        minValue: currentTemperatureValue.min,
+        maxValue: currentTemperatureValue.max,
+      })
+      .updateValue(currentTemperatureValue.min);
+
+    const targetTemperatureValue = device.deviceModel.value('TempCfg') as RangeValue;
+    this.service.getCharacteristic(Characteristic.CoolingThresholdTemperature)
+      .setProps({
+        minValue: targetTemperatureValue.min,
+        maxValue: targetTemperatureValue.max,
+      })
+      .updateValue(targetTemperatureValue.min);
+    this.service.getCharacteristic(Characteristic.HeatingThresholdTemperature)
+      .setProps({
+        minValue: targetTemperatureValue.min,
+        maxValue: targetTemperatureValue.max,
+      })
+      .updateValue(targetTemperatureValue.min);
+  }
+
   async setActive(value: CharacteristicValue) {
     const device: Device = this.accessory.context.device;
     const isOn = value as boolean;
     const op = isOn ? ACOperation.RIGHT_ON : ACOperation.OFF;
-    const opValue = device.deviceModel.enumValue('OpMode', op);
+    const opValue = device.deviceModel.enumValue('Operation', op);
 
-    await this.platform.ThinQ?.thinq1DeviceControl(device.id, 'OpMode', opValue);
+    await this.platform.ThinQ?.thinq1DeviceControl(device.id, 'Operation', opValue);
   }
 
   async setTargetTemperature(value: CharacteristicValue) {
