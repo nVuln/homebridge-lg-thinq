d53da9b905186e8766499f988c676cf6ae13b79a
Jin <j@shopar.dev>
2022-06-12 13:01:47 +0700

    turbo mode for model AIR_910604_WW

--- src/v1/devices/AirPurifier.ts
+++ src/v1/devices/AirPurifier.ts
@@ -1,8 +1,18 @@
 import {default as V2, RotateSpeed} from '../../devices/AirPurifier';
-import {CharacteristicValue} from 'homebridge';
+import {CharacteristicValue, PlatformAccessory} from 'homebridge';
 import {Device} from '../../lib/Device';
+import {LGThinQHomebridgePlatform} from '../../platform';
 
-export default class AirPurifier extends V2{
+export default class AirPurifier extends V2 {
+  constructor(
+    protected readonly platform: LGThinQHomebridgePlatform,
+    protected readonly accessory: PlatformAccessory,
+  ) {
+    super(platform, accessory);
+
+    this.turboModeSetup(accessory.context.device);
+  }
+
   async setActive(value: CharacteristicValue) {
     const device: Device = this.accessory.context.device;
     await this.platform.ThinQ?.thinq1DeviceControl(device, 'Operation', value as boolean ? '1' : '0');
@@ -44,5 +54,31 @@ export default class AirPurifier extends V2{
 
     const device: Device = this.accessory.context.device;
     await this.platform.ThinQ?.thinq1DeviceControl(device, 'SignalLighting', value as boolean ? '1' : '0');
+  }
+
+  public turboModeSetup(device: Device) {
+    // model supported only
+    if (['AIR_910604_WW'].includes(device.model)) {
+      const {
+        Service: {
+          Switch,
+        },
+      } = this.platform;
+
+      const serviceTurboMode = this.accessory.getService(Switch) || this.accessory.addService(Switch, 'Turbo Mode');
+      serviceTurboMode.updateCharacteristic(this.platform.Characteristic.Name, 'Turbo Mode');
+      serviceTurboMode.getCharacteristic(this.platform.Characteristic.On)
+        .onGet(() => {
+          return this.accessory.context.device.snapshot.raw['AirFast'];
+        })
+        .onSet((value: CharacteristicValue) => {
+          if (this.Status.isPowerOn) {
+            this.platform.ThinQ.thinq1DeviceControl(device, 'AirFast', value ? '1' : '0').then(() => {
+              device.data.snapshot.raw['AirFast'] = value ? 1 : 0;
+              this.updateAccessoryCharacteristic(device);
+            });
+          }
+        });
+    }
   }
 }
