a295be2a64fcc49075e567de9c3bafb156c56650
Jin <j@shopar.dev>
2022-09-23 23:17:28 +0700

    added dishwasher door status & event finished as occupancy sensor

--- src/devices/Dishwasher.ts
+++ src/devices/Dishwasher.ts
@@ -5,7 +5,11 @@ import {Device} from '../lib/Device';
 import {WasherDryerStatus} from './WasherDryer';
 
 export default class Dishwasher extends baseDevice {
+  public isRunning = false;
+
   protected serviceDishwasher;
+  protected serviceDoorOpened;
+  protected serviceEventFinished;
 
   constructor(
     protected readonly platform: LGThinQHomebridgePlatform,
@@ -16,6 +20,8 @@ export default class Dishwasher extends baseDevice {
     const {
       Service: {
         Valve,
+        ContactSensor,
+        OccupancySensor,
       },
       Characteristic,
     } = this.platform;
@@ -32,6 +38,18 @@ export default class Dishwasher extends baseDevice {
     this.serviceDishwasher.getCharacteristic(Characteristic.RemainingDuration).setProps({
       maxValue: 86400, // 1 day
     });
+
+    // Door open state
+    this.serviceDoorOpened = accessory.getService(ContactSensor) || accessory.addService(ContactSensor, 'Dishwasher Door');
+
+    this.serviceEventFinished = accessory.getService(OccupancySensor);
+    if (this.config.dishwasher_trigger as boolean) {
+      this.serviceEventFinished = this.serviceEventFinished || accessory.addService(OccupancySensor, device.name + ' - Program Finished');
+      // eslint-disable-next-line max-len
+      this.serviceEventFinished.updateCharacteristic(Characteristic.OccupancyDetected, Characteristic.OccupancyDetected.OCCUPANCY_NOT_DETECTED);
+    } else if (this.serviceEventFinished) {
+      accessory.removeService(this.serviceEventFinished);
+    }
   }
 
   public setActive() {
@@ -49,16 +67,67 @@ export default class Dishwasher extends baseDevice {
     }
     this.serviceDishwasher.updateCharacteristic(Characteristic.Active, this.Status.isPowerOn ? 1 : 0);
     this.serviceDishwasher.updateCharacteristic(Characteristic.InUse, this.Status.isRunning ? 1 : 0);
+
+    if (this.serviceDoorOpened) {
+      const contactSensorValue = this.Status.isDoorClosed ?
+        Characteristic.ContactSensorState.CONTACT_DETECTED : Characteristic.ContactSensorState.CONTACT_NOT_DETECTED;
+      this.serviceDoorOpened.updateCharacteristic(Characteristic.ContactSensorState, contactSensorValue);
+    }
   }
 
   public get Status() {
     return new DishwasherStatus(this.accessory.context.device.snapshot?.dishwasher, this.accessory.context.device.deviceModel);
+  }
+
+  public get config() {
+    return Object.assign({}, {
+      dishwasher_trigger: false,
+    }, super.config);
+  }
+
+  public update(snapshot) {
+    super.update(snapshot);
+
+    const dishwasher = snapshot.dishwasher;
+    if (!dishwasher) {
+      return;
+    }
+
+    // when washer state is changed
+    if (this.config.dishwasher_trigger as boolean && this.serviceEventFinished && 'state' in dishwasher) {
+      const {
+        Characteristic: {
+          OccupancyDetected,
+        },
+      } = this.platform;
+
+      // detect if washer program in done
+      if ((['END'].includes(dishwasher.state)) || (this.isRunning && !this.Status.isRunning)) {
+        this.serviceEventFinished.updateCharacteristic(OccupancyDetected, OccupancyDetected.OCCUPANCY_DETECTED);
+        this.isRunning = false; // marked device as not running
+
+        // turn it off after 10 minute
+        setTimeout(() => {
+          this.serviceEventFinished.updateCharacteristic(OccupancyDetected, OccupancyDetected.OCCUPANCY_NOT_DETECTED);
+        }, 10000 * 60);
+      }
+
+      // detect if dishwasher program is start
+      if (this.Status.isRunning && !this.isRunning) {
+        this.serviceEventFinished.updateCharacteristic(OccupancyDetected, OccupancyDetected.OCCUPANCY_NOT_DETECTED);
+        this.isRunning = true;
+      }
+    }
   }
 }
 
 // re-use some status in washer
 export class DishwasherStatus extends WasherDryerStatus {
   public get isRunning() {
-    return this.isPowerOn && this.data?.state === 'RUNNING';
+    return this.isPowerOn && this.data?.state === this.deviceModel.lookupMonitorName('state', '@DW_STATE_RUNNING_W');
+  }
+
+  public get isDoorClosed() {
+    return this.data?.door === this.deviceModel.lookupMonitorName('door', '@CP_OFF_EN_W');
   }
 }
